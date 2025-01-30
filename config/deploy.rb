30a881362dc034a7e7be03f8d8314444bb2442ee
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2025-01-30 13:08:14 +0100

    Fix deployment stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11"
 
-set :application, "perp_web"
+set :application, "melder"
 set :repo_url, "git@github.com:tillsc/melder.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, 'main'
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/vhosts/perp.de/melder.perp.de/app"
